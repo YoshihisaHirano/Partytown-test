983c44b01b90fe87428b4d1fed677e2c49b8b9ce
Anastasia Zubkova <anastasiia.zubkova@bejamas.io>
2023-02-04 17:38:22 +0400

    adds-partytown-resolv-url-for-proxying

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,8 +2,16 @@ import React from "react"
 import { Partytown } from '@builder.io/partytown/react'
 
 export const onRenderBody = ({ setHeadComponents }) => {
+    const resolveUrl  = (url, location, type) => {
+        const baseUrl = new URL('https://main--flourishing-brigadeiros-99374f.netlify.app/cors-proxy/');
+        if (type === 'script') {
+            const proxyUrl = new URL(url, baseUrl);
+            return proxyUrl;
+        }
+        return url;
+    }
     setHeadComponents([
-        <Partytown key="partytown" debug={true} forward={['window.dataLayer.push']} />,
+        <Partytown key="partytown" resolveUrl={resolveUrl} debug={true} forward={['window.dataLayer.push']} />,
         <script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-B6SXD335KF" key="blocking-script"></script>,
         <script key="executed-code" dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
