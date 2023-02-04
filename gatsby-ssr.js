a47c45bdcb25cedcda92beaf128b576220e0bec5
Anastasia Zubkova <anastasiia.zubkova@bejamas.io>
2023-02-04 16:30:11 +0400

    gtag-with-partytown

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,10 @@
-const React = require("react")
+import React from "react"
+import { Partytown } from '@builder.io/partytown/react'
 
-exports.onRenderBody = ({ setHeadComponents }) => {
+export const onRenderBody = ({ setHeadComponents }) => {
     setHeadComponents([
-        <script src="https://www.googletagmanager.com/gtag/js?id=G-B6SXD335KF" key="blocking-script"></script>,
+        <Partytown key="partytown" debug={true} forward={['window.dataLayer.push']} />,
+        <script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-B6SXD335KF" key="blocking-script"></script>,
         <script key="executed-code" dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
             function gtag(){window.dataLayer.push(arguments)}
